6964f47129150b0d7e47011f57724a60aa1b7b81
Dimitri <56205878+PurgeStudios@users.noreply.github.com>
2023-10-02 20:20:16 -0400

    Basic Structure
    Added Code:
    User Input
    Row + Column

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    //---------------------------------------------------
+    // Assigning User's Input to number of Stars
+    //---------------------------------------------------
+
+    System.out.println("Please Enter a Number:");
+    Scanner input = new Scanner(System.in);
+    int stars = input.nextInt();
+
+    //---------------------------------------------------
+    // Code for Row and Colomn of Stars
+    //---------------------------------------------------
+
+    for(int r = 0; r < stars; ++r) {
+      for(int c = 0; c < stars; ++c)
+        System.out.print("*");
+
+    System.out.println();
+    input.close();
+    }
   }
 }
