8daa781d7c6d3593373730c6c84b0b38b89a4773
Dimitri <56205878+PurgeStudios@users.noreply.github.com>
2023-10-16 15:37:29 -0400

    Updated Comments

--- Diamond.java
+++ Diamond.java
@@ -4,18 +4,18 @@ public class Diamond {
   public static void main(String[] args) {
     System.out.println("Please Enter a Number:");
     Scanner input = new Scanner(System.in);
-
+    //-----------------------------------------
     // Assigning Variables
-
+    //-----------------------------------------
     int numOfStars = input.nextInt();
     int shapeOfDiamond, rows;
-
+    //-----------------------------------------
     // Code checking if user input is negative
-
+    //-----------------------------------------
     if(numOfStars%2 == 1) {
-
+    //-----------------------------------------
     // Code for top of Odd Diamond
-
+    //-----------------------------------------
       for(rows = 1; rows < numOfStars; rows += 2) {
         for(shapeOfDiamond = 0; shapeOfDiamond < numOfStars - 1 - rows/2; ++shapeOfDiamond) {
           System.out.print(" ");
@@ -25,9 +25,9 @@ public class Diamond {
         }
         System.out.println("");
       }
-
+      //-----------------------------------------
       // Code for bottom of Odd Diamond
-
+      //-----------------------------------------
       for(rows = numOfStars; rows > 0; rows -= 2) {
         for(shapeOfDiamond = 0; shapeOfDiamond < numOfStars - 1 - rows/2; ++shapeOfDiamond) {
           System.out.print(" ");
@@ -38,12 +38,13 @@ public class Diamond {
       System.out.println("");
       }
     }
+      //-----------------------------------------
       // Code checking if user input is positive
-
+      //-----------------------------------------
       else if(numOfStars%2 == 0) {
-
-      // Attempt at Code for top of even diamond
-
+        //-----------------------------------------
+        // Attempt at Code for top of even diamond
+        //-----------------------------------------
       for(rows = 2; rows <= numOfStars / 2 + 1; rows += 1) {
         for(int spaces = -2 * rows + (numOfStars + 2); spaces < 0; spaces -= 1) {
           System.out.println(" ");
@@ -54,9 +55,9 @@ public class Diamond {
         System.out.println("");
       }
       System.out.println("");
-
+        //-----------------------------------------
         //Code for bottom of even diamond
-
+        //-----------------------------------------
       for(rows = numOfStars / 2 + 1; rows <= numOfStars/ 2 + 1; rows += 1) {
         for(int spaces = -2 * rows + (numOfStars + 2); spaces < 0; spaces -= 1) {
           System.out.println(" ");
